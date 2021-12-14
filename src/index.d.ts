de70ff8ecfd5525b0a8119e5f0a9e23ee224b78a
Hoang Lam <hoanglam10499@gmail.com>
2021-12-14 19:17:54 +0700

    update

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Constructor, ViewProps, NativeMethodsMixin } from 'react-native';
-import React from 'react';
-
-export interface PropsView extends ViewProps {}
-declare class DropShadowComponent extends React.Component<PropsView> {}
-declare const DropShadowBase: Constructor<NativeMethodsMixin> &
-  typeof DropShadowComponent;
-
-export default class DropShadow extends DropShadowBase {}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Platform, requireNativeComponent, View } from 'react-native';
-export default (props) => {
-  if (Platform.OS === 'android') {
-    const ShadowView = requireNativeComponent('DropShadow');
-    return <ShadowView {...props} />;
-  }
-  return <View {...props} />;
-};
